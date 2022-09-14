1c4239a7441cd6504d3df0a7e3ab465b960c58f8
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-09-14 00:15:15 -0300

    chore: complet components division

---
+++ src/Stories.js
@@ -0,0 +1,18 @@
+export default function Stories() {
+    return (
+        <div class="stories">
+            <div class="story">
+                <div class="imagem">
+                    <img src="assets/img/9gag.svg" />
+                </div>
+                <div class="usuario">
+                    9gag
+                </div>
+            </div>
+
+            <div class="setinha">
+                <ion-icon name="chevron-forward-circle"></ion-icon>
+            </div>
+        </div>
+    );
+}
